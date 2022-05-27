b788fe9f63e32e34f46eb8e643362da4eb729c6a
ashaypatil11 <97874406+ashaypatil11@users.noreply.github.com>
2022-05-28 02:34:39 +0530

    Update Spark_Hadoop.py

--- Spark_Hadoop.py
+++ Spark_Hadoop.py
@@ -30,7 +30,7 @@ spark.sql("select userID, name from peoples where friends > 100 order by userID"
 
 output.write\
 .format("json").mode("overwrite")\
-.option("path", "hdfs:///user/maria_dev/spark/job-output")\
+.option("path", "hdfs:///user/maria_dev/spark/job_output/")\
 .partitionBy("age")\
 .save()
 
