fb21af854a57f8d46bf46362dca6134bbca484a3
ashaypatil11 <97874406+ashaypatil11@users.noreply.github.com>
2022-06-22 21:43:29 +0530

    Add files via upload

---
+++ mongospark.py
@@ -0,0 +1,43 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql import functions
+
+def parseInput(line):
+    fields = line.split('|')
+    return Row(user_id = int(fields[0]), age = int(fields[1]), gender = fields[2], occupation = fields[3], zip = fields[4])
+
+if __name__ == "__main__":
+    # Create a SparkSession
+    spark = SparkSession.\
+    builder.appName("MongoDBIntegration").\
+    getOrCreate()
+
+    # Build RDD on top of users data file
+    lines = spark.sparkContext.textFile("hdfs:///user/maria_dev/mongodb/movies.user")
+    
+    # Creating new RDD by passing the parser fuction
+    users = lines.map(parseInput)
+    
+    # Convert RDD into a DataFrame
+    usersDataset = spark.createDataFrame(users)
+
+    # Write the data into MongoDB
+    usersDataset.write\
+        .format("com.mongodb.spark.sql.DefaultSource")\
+        .option("uri","mongodb://127.0.0.1/movielens.users")\
+        .mode('append')\
+        .save()
+
+    # Read it back from MongoDB into a new Dataframe
+    readUsers = spark.read\
+    .format("com.mongodb.spark.sql.DefaultSource")\
+    .option("uri","mongodb://127.0.0.1/movielens.users")\
+    .load()
+
+    readUsers.createOrReplaceTempView("users")
+
+    sqlDF = spark.sql("SELECT * FROM users WHERE age < 20")
+    sqlDF.show()
+
+    # Stop the SparkSession
+    spark.stop()
