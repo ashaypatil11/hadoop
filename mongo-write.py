1afe1da6d78cbaee9e916e6e9918fe82ee7917d7
ashaypatil11 <97874406+ashaypatil11@users.noreply.github.com>
2023-02-26 17:00:51 +0530

    Update mongo-write.py

--- mongo-write.py
+++ mongo-write.py
@@ -4,20 +4,26 @@ from pyspark.sql import functions
 
 def parseInput(line):
     fields = line.split('|')
-    return Row(user_id = int(fields[0]), age = int(fields[1]), gender = fields[2], occupation = fields[3], zip = fields[5])
+    return Row(user_id = int(fields[0]), age = int(fields[1]), gender = fields[2], occupation = fields[3], zip = fields[4])
 
-spark = SparkSession.\
-    builder.appName("mongo-spark").\
-        getOrCreate()
+if __name__ == "__main__":
+    # Create a SparkSession
+    spark = SparkSession.\
+    builder.appName("MongoDBIntegration").\
+    getOrCreate()
 
-lines = spark.sparkContext.textFile("hdfs:///user/maria_dev/mongodb/movies.user")
+    # Build RDD on top of users data file
+    lines = spark.sparkContext.textFile("hdfs:///user/maria_dev/mongodb/movies.user")
+    
+    # Creating new RDD by passing the parser fuction
+    users = lines.map(parseInput)
+    
+    # Convert RDD into a DataFrame
+    usersDataset = spark.createDataFrame(users)
 
-users = lines.map(parseInput)
-
-userDataset = spark.createDataFrame(users)
-
-userDataset.write.\
-    format("com.mongodb.spark.sql.DefaultSource").\
-        option("uri", "mongodb://127.0.0.1/moviesdata.user").\
-            mode("overwrite").\
-                save()
+    # Write the data into MongoDB
+    usersDataset.write\
+        .format("com.mongodb.spark.sql.DefaultSource")\
+        .option("uri","mongodb://127.0.0.1/moviesdata.users")\
+        .mode('append')\
+        .save()
