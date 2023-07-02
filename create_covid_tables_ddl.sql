97c8e8c2127198a68c3677633544a315a8306a3a
ashaypatil11 <97874406+ashaypatil11@users.noreply.github.com>
2023-07-03 00:25:01 +0530

    Add files via upload

---
+++ create_covid_tables_ddl.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE covid_reporting.population_by_age
+(
+    country_code            VARCHAR(2),
+    age_group_0_14          VARCHAR(5),
+    age_group_15_24         VARCHAR(5),
+    age_group_25_49         VARCHAR(5),
+    age_group_50_64         VARCHAR(5),
+    age_group_65_79         VARCHAR(5),
+    age_group_80_max        VARCHAR(5)
+)
+GO
+
